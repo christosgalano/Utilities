6a6d2aacb97130bc48dad422a7bf5d1236c80771
Christos Galanopoulos <61470783+christosgalano@users.noreply.github.com>
2021-02-12 23:08:11 +0200

    Update Timer.hpp

--- Timer/Timer.hpp
+++ Timer/Timer.hpp
@@ -2,6 +2,12 @@
 #include <iostream>
 #include <chrono>
 
+// Simple Timer class
+// Example of usage:
+// Timer t;
+// foo();
+// t.stop();
+
 class Timer {
 private:
 	std::chrono::time_point<std::chrono::steady_clock> m_StartTimepoint;
@@ -10,12 +16,8 @@ public:
 	Timer() {
 		m_StartTimepoint = std::chrono::steady_clock::now();
 	}
-
-	~Timer() {
-		Stop();
-	}
-
-	void Stop() {
+	
+	void stop() {
 		m_EndTimepoint = std::chrono::steady_clock::now();
 		auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(m_EndTimepoint - m_StartTimepoint).count();
 		std::cout << "Duration: " << duration << " ms\n";
