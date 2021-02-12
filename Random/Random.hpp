ed7b50cc4013ad9d52f3c81a5619b109e9490d76
Christos Galanopoulos <61470783+christosgalano@users.noreply.github.com>
2021-02-12 23:01:21 +0200

    Update Random.hpp

--- Random/Random.hpp
+++ Random/Random.hpp
@@ -1,22 +1,15 @@
 #include <random>
 #include <chrono>
 
+// Simple number generator classes
+
 class RandomInt {
 private:
     std::default_random_engine re;
     std::uniform_int_distribution<> dist;
+    std::random_device rd;
 public:
-    // We use as seed the time the class gets constructed to avoid same results on
-    // multiple calls with same bounds.
-    // We could use std::random_device but then we get an error when we run with Valgrind. That's because
-    // when the Ubuntu system has a random number library which uses the RDRAND instruction,
-    // older versions of Valgrind do not recognise that instruction.
-    // So Valgrind will stumble on an unknown instruction and will not be able to
-    // translate it into the VEX intermediate language. This fixed with newer versions like 3.16.1.
-
-    RandomInt(int low, int high) : dist{low, high} { 
-        re.seed(std::chrono::system_clock::now().time_since_epoch().count()); 
-    }
+    RandomInt(int low, int high) : re{rd()}, dist{low, high} {}
 
     int operator()() { return dist(re); }  // Get a random integer in [low:high]
 
@@ -29,10 +22,9 @@ class RandomDouble {
 private: 
     std::default_random_engine re;
     std::uniform_real_distribution<double> dist;
+    std::random_device rd;
 public:
-    RandomDouble(double low, double high) : dist{low, high} {
-        re.seed(std::chrono::system_clock::now().time_since_epoch().count());         
-    }
+    RandomDouble(double low, double high) : re{rd()}, dist{low, high} {}
 
     double operator()() { return dist(re); }  // Get a random double in [low:high)
     
